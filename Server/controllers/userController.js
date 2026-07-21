b936b01f4c9b36b5e2dc29d64a9f952eed5cb66f
ALTAMASH732300 <patelaltamsh956@gmail.com>
2026-07-21 16:32:31 +0530

    Complete authentication and user management module

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,4 +1,5 @@
 const client = require("../database");
+const bcrypt=require("bcrypt");
 
 
 const getProfile = (req, res) => {
@@ -47,4 +48,79 @@ const UpdateProfile=async(req,res)=>{
     }
 
 }
-module.exports={UpdateProfile,getProfile};
+
+const UpdatePassword=async(req,res)=>{
+    const {currentPassword,newPassword,confirmPassword}=req.body;
+    const userId=req.user.id;
+    if(!currentPassword.trim() || !newPassword.trim() || !confirmPassword.trim())
+        return res.status(400).json({
+    success:false,
+    message:"Enter All the Required Data"
+    })
+    else{
+        if(newPassword !==confirmPassword)
+        {
+            return res.status(400).json({
+                success:false,
+                message:"New password and confirm password are not matched"
+            })
+        }
+        if(currentPassword===newPassword)
+            return res.json({
+        success:false,
+        message:"New password must be different from the current password."
+        })
+    }
+    
+    const query1="select password from users where id=$1";
+    const query2="update users set password=$1 where id=$2"
+    try{
+    const result1=await client.query(query1,[userId]);
+
+    if(result1.rowCount===0)
+        return res.status(404).json({
+    success:false,
+    message:"User not found"
+    })
+    const storedPassword = result1.rows[0].password;
+    const isMatch = await bcrypt.compare(
+      currentPassword,
+      storedPassword
+    );
+    if(!isMatch)
+    {
+        return res.status(401).json({
+            success:false,
+            message:"Enters password is wrong"
+        })
+    }
+    const newHashedPassword=await bcrypt.hash(newPassword,10)
+
+    const result2=await client.query(query2,[newHashedPassword,userId]);
+    if(result2.rowCount===0)
+    {
+        return res.status(500).json({
+            success:false,
+            message:"Internal Database Problem"
+        })
+    }
+    else{
+        return res.status(200).json({
+            success:true,
+            message:"Password Updated Successfully"
+        })
+    }
+
+    
+    }
+    catch(err){
+        console.error(err);
+        return res.status(500).json({
+            success:false,
+            message:"Internal server error"
+        })
+
+    }
+
+}
+module.exports={UpdateProfile,getProfile,UpdatePassword};
--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -8,11 +8,12 @@ router.get("/",(req,res)=>{
 const authMiddleware = require("../middleware/authMiddleware");
 
 const { register, login } = require("../controllers/authController");
-const { UpdateProfile,getProfile } = require("../controllers/userController");
+const { UpdateProfile,getProfile,UpdatePassword} = require("../controllers/userController");
 router.post("/registration",register); 
 
 router.post("/login", login)    //login route
 
 router.get("/profile", authMiddleware, getProfile); //get profile
 router.put("/profile",authMiddleware,UpdateProfile);
+router.put("/changePassword",authMiddleware,UpdatePassword);
 module.exports = router;
